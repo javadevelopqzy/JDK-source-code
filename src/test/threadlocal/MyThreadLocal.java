a47e1feb166242f02b182366312b4f8e61de11e7
qzy <1252525229@qq.com>
2018-12-10 23:46:54 +0800

    补充ThreadLocal解析代码

---
+++ src/test/threadlocal/MyThreadLocal.java
@@ -0,0 +1,16 @@
+package test.threadlocal;
+
+public class MyThreadLocal<T> extends ThreadLocal<T> {
+
+	String name;
+
+	public MyThreadLocal(String name) {
+		this.name = name;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		System.out.println(name + "已经被回收");
+	}
+}
